b3b6acbb47c4c67712ec4cd0d78f8365b9945e69
Jeroen Branders <j.branders@tretton.eu>
2022-12-21 15:52:27 +0100

    Update mongodb connection

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 4000;
 
 // Mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/productsdb', {
+mongoose.connect('mongodb://localhost/productsdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
