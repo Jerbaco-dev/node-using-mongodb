2c020a42db1a74e0b759d41bb9f67bf942a02e63
Jeroen Branders <j.branders@tretton.eu>
2022-12-21 15:48:35 +0100

    Update index with mongo connection

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 4000;
 
 // Mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://cosmos-humanbox-dev:01UixUkWPf84nZqXFvimJ1re61GFHoO3WMObjk4K6JRtGqIH4c7PAK2S4tq3RoOV2lSp5B67O7rqACDb2iKwBQ==@cosmos-humanbox-dev.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-humanbox-dev@', {
+mongoose.connect('mongodb://localhost:27017/productsdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
