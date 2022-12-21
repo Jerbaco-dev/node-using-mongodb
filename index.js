8d0c6d71ff5fd46e66a080e4080d4c536e1afb81
Jeroen Branders <j.branders@tretton.eu>
2022-12-21 14:17:04 +0100

    connection to cosmosdb

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
+
+// Mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://cosmos-humanbox-dev:01UixUkWPf84nZqXFvimJ1re61GFHoO3WMObjk4K6JRtGqIH4c7PAK2S4tq3RoOV2lSp5B67O7rqACDb2iKwBQ==@cosmos-humanbox-dev.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmos-humanbox-dev@', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
